baf742a03df317a1aae7ca25cce05a64d7e22d84
ostrebler <strebler.olivier@hotmail.fr>
2020-04-21 23:09:20 +0200

    More complete warning & failure logging

--- src/report/Report.ts
+++ src/report/Report.ts
@@ -1,10 +1,12 @@
 import { codeFrameColumns } from "@babel/code-frame";
 import lineColumn from "line-column";
 import joinn from "joinn";
-import { groupBy, values, sortBy } from "lodash";
+import { groupBy, lowerCase, values, sortBy } from "lodash";
 import {
   Failure,
   Internals,
+  PredicateFailure,
+  SemanticFailure,
   SemanticWarning,
   TerminalFailure,
   Warning
@@ -38,7 +40,6 @@ export class Report<TContext> {
 
   log() {
     const lineReader = lineColumn(this.input);
-
     const factored = sortBy(
       values(
         groupBy(
@@ -81,7 +82,7 @@ export class Report<TContext> {
                           : "start of input";
                       case "TOKEN":
                         return failure.identity
-                          ? `token ${failure.identity}`
+                          ? lowerCase(failure.identity)
                           : "unnamed token";
                     }
                   }
@@ -91,61 +92,28 @@ export class Report<TContext> {
               )
           );
 
-        return codeFrameColumns(
-          this.input,
-          {
+        if (packed["PREDICATE_FAILURE"])
+          message.push(
+            (packed["PREDICATE_FAILURE"] as PredicateFailure[])
+              .map(() => "Failure: Predicate failed")
+              .join("\n")
+          );
+
+        if (packed["SEMANTIC_FAILURE"])
+          message.push(
+            (packed["SEMANTIC_FAILURE"] as SemanticFailure[])
+              .map(({ message }) => `Failure: ${message}`)
+              .join("\n")
+          );
+
+        return [
+          message.join("\n"),
+          codeFrameColumns(this.input, {
             start: { line: from.line, column: from.col },
             end: { line: to.line, column: to.col }
-          },
-          { message: message.join("\n") }
-        );
+          })
+        ].join("\n");
       })
       .join("\n\n");
-
-    /*const breakInput = (from: number, to: number) =>
-      this.input.substring(from, to).replace("\n", "\\n");
-
-    const highlight = (info: string, from: number, to: number) => {
-      const before = breakInput(from - 10, from);
-      const inner = breakInput(from, to);
-      const after = breakInput(to, to + 10);
-      const { line, col } = reader.fromIndex(from)!;
-      const pointer = `${" ".repeat(before.length)}${"^".repeat(
-        inner.length || 1
-      )}`;
-      return `Line ${line}:${col}: ${info}\n${before}${inner}${after}\n${pointer}`;
-    };
-
-    return [...this.warnings, ...this.failures]
-      .map(log => {
-        let info = "";
-        switch (log.type) {
-          case "SEMANTIC_WARNING":
-          case "SEMANTIC_FAILURE":
-            info = log.message;
-            break;
-          case "TERMINAL_FAILURE":
-            info =
-              log.terminal === "LITERAL"
-                ? `Expected literal "${log.literal}"`
-                : log.terminal === "REGEX"
-                ? `Expected match with ${log.pattern.toString()}`
-                : log.terminal === "BOUND"
-                ? `Expected ${log.bound === "START" ? "start" : "end"} of input`
-                : "";
-            break;
-          case "TOKEN_FAILURE":
-            info = `Expected token <${log.identity || "anonymous"}>`;
-            break;
-          case "PREDICATE_FAILURE":
-            info = `Predicate failed (should ${
-              log.polarity ? "" : "not "
-            }have matched)`;
-            break;
-        }
-        info += ` [${log.stack.join(" > ")}]`;
-        return highlight(info, log.from, log.to);
-      })
-      .join("\n");*/
   }
 }
