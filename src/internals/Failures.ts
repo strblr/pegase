2e91ce6aec09eaceee1669d321a6cfebb58e350d
ostrebler <strebler.olivier@hotmail.fr>
2020-04-21 23:08:08 +0200

    Bug correction

--- src/internals/Failures.ts
+++ src/internals/Failures.ts
@@ -19,7 +19,6 @@ export class Failures {
   write(failure: Failure) {
     const chunk = this.chunks[0];
     if (chunk.length === 0 || chunk[0].to === failure.to) chunk.push(failure);
-    else if (chunk[0].to < failure.to)
-      chunk.splice(0, this.chunks.length, failure);
+    else if (chunk[0].to < failure.to) chunk.splice(0, chunk.length, failure);
   }
 }
