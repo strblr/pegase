8b4d348674c0f28e61d40a05b9696f33fc0034e6
ostrebler <strebler.olivier@hotmail.fr>
2021-05-31 05:48:26 +0200

    Added directives

--- src/peg.ts
+++ src/peg.ts
@@ -1,4 +1,38 @@
-import { Parser, SemanticAction } from ".";
+import assign from "lodash/assign";
+import isArray from "lodash/isArray";
+import {
+  ActionParser,
+  Directives,
+  LiteralParser,
+  OptionMergeParser,
+  OptionParser,
+  Parser,
+  PegTemplateArg,
+  TokenParser
+} from ".";
+
+export const directives: Directives = {
+  raw: parser => new ActionParser(parser, ({ $raw }) => $raw),
+  omit: parser => new ActionParser(parser, () => 45),
+  token: parser => new TokenParser(parser),
+  skip: parser => new OptionMergeParser(parser, { skip: true }),
+  noskip: parser => new OptionMergeParser(parser, { skip: false }),
+  case: parser => new OptionMergeParser(parser, { ignoreCase: false }),
+  nocase: parser => new OptionMergeParser(parser, { ignoreCase: true }),
+  count: parser =>
+    new ActionParser(parser, ({ $value }) =>
+      isArray($value) ? $value.length : -1
+    ),
+  test: parser =>
+    new OptionParser([
+      new ActionParser(parser, () => true),
+      new ActionParser(new LiteralParser("", false), () => false)
+    ])
+};
+
+export function extendDirectives(addons: Directives) {
+  assign(directives, addons);
+}
 
 /** The peg metagrammar
  *
@@ -8,12 +42,6 @@ import { Parser, SemanticAction } from ".";
  *
  */
 
-export type PegTemplateArg<Context> =
-  | string
-  | RegExp
-  | Parser<any, Context>
-  | SemanticAction<any, Context>;
-
 export function peg<Value = any, Context = any>(
   chunks: TemplateStringsArray,
   ...args: Array<PegTemplateArg<Context>>
@@ -21,8 +49,10 @@ export function peg<Value = any, Context = any>(
   return {} as Parser<Value, Context>;
 }
 
+/*
 const math = peg`
   expr: <first>term <rest>(("+" | "-") term)*
   term: <first>fact <rest>(("*" | "/") fact)*
   fact: 
 `;
+*/
