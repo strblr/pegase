89b42a14dcf0155b02c6122f1f7c8adc99eef975
ostrebler <strebler.olivier@hotmail.fr>
2021-10-11 03:25:57 +0200

    Browser demo version should have direct access to hooks

--- src/index.browser.ts
+++ src/index.browser.ts
@@ -3,5 +3,19 @@ import * as pegase from ".";
 if (typeof window !== "undefined") {
   const w = window as any;
   w.peg = pegase.peg;
-  w._ = pegase;
+  w.$from = pegase.$from;
+  w.$to = pegase.$to;
+  w.$children = pegase.$children;
+  w.$value = pegase.$value;
+  w.$raw = pegase.$raw;
+  w.$options = pegase.$options;
+  w.$context = pegase.$context;
+  w.$warn = pegase.$warn;
+  w.$fail = pegase.$fail;
+  w.$expected = pegase.$expected;
+  w.$commit = pegase.$commit;
+  w.$emit = pegase.$emit;
+  w.$node = pegase.$node;
+  w.$visit = pegase.$visit;
+  w.$parent = pegase.$parent;
 }
