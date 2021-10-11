2fa04d07f272d42cf4f4adbef0bc8dba2428e628
ostrebler <strebler.olivier@hotmail.fr>
2021-10-11 06:50:48 +0200

    Rewrote index.browser.ts, corrected doc

--- src/index.browser.ts
+++ src/index.browser.ts
@@ -1,21 +1,23 @@
 import * as pegase from ".";
 
 if (typeof window !== "undefined") {
-  const w = window as any;
-  w.peg = pegase.peg;
-  w.$from = pegase.$from;
-  w.$to = pegase.$to;
-  w.$children = pegase.$children;
-  w.$value = pegase.$value;
-  w.$raw = pegase.$raw;
-  w.$options = pegase.$options;
-  w.$context = pegase.$context;
-  w.$warn = pegase.$warn;
-  w.$fail = pegase.$fail;
-  w.$expected = pegase.$expected;
-  w.$commit = pegase.$commit;
-  w.$emit = pegase.$emit;
-  w.$node = pegase.$node;
-  w.$visit = pegase.$visit;
-  w.$parent = pegase.$parent;
+  const keys: (keyof typeof pegase)[] = [
+    "peg",
+    "$from",
+    "$to",
+    "$children",
+    "$value",
+    "$raw",
+    "$options",
+    "$context",
+    "$warn",
+    "$fail",
+    "$expected",
+    "$commit",
+    "$emit",
+    "$node",
+    "$visit",
+    "$parent"
+  ];
+  for (const key of keys) (window as any)[key] = pegase[key];
 }
