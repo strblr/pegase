4666cbf87cf04241c9cbb5ad300a2f44031a4794
ostrebler <strebler.olivier@hotmail.fr>
2021-10-11 02:21:34 +0200

    Edited tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -458,26 +458,29 @@ test("Benchmark between Pegase and competitor", () => {
     competitor.parse("42 +  63 * (12 / 3)");
   }
   const c = new Date();
-  console.log(b.getTime() - a.getTime(), "ms");
-  console.log(c.getTime() - b.getTime(), "ms");
+  console.log(
+    b.getTime() - a.getTime(),
+    "ms vs.",
+    c.getTime() - b.getTime(),
+    "ms"
+  );
 });
 
 test("Math expressions should be correctly calculated", () => {
-  for (let i = 0; i !== 100; ++i) {
-    const doop = (left: number, op: string, right: number) => {
-      switch (op) {
-        case "+":
-          return left + right;
-        case "-":
-          return left - right;
-        case "*":
-          return left * right;
-        case "/":
-          return left / right;
-      }
-    };
+  const doop = (left: number, op: string, right: number) => {
+    switch (op) {
+      case "+":
+        return left + right;
+      case "-":
+        return left - right;
+      case "*":
+        return left * right;
+      case "/":
+        return left / right;
+    }
+  };
 
-    const calc = peg<number>`
+  const calc = peg<number>`
       expr: term % ("+" | "-") @infix(${doop})
       term: fact % ("*" | "/") @infix(${doop})
       fact: number | '(' expr ')'
@@ -485,57 +488,56 @@ test("Math expressions should be correctly calculated", () => {
         '-'? [0-9]+ ('.' [0-9]*)?
     `;
 
-    expect(calc.value("2 + 3")).toBe(5);
-    expect(calc.value("2 * 3")).toBe(6);
-    expect(calc.value("2 * -3")).toBe(-6);
-    expect(calc.value("89")).toBe(89);
-    expect(calc.value("2.53")).toBe(2.53);
-    expect(calc.value("-1.2")).toBe(-1.2);
-    expect(calc.test("")).toBe(false);
-    expect(calc.test("1 +")).toBe(false);
-    expect(calc.test("(1 +")).toBe(false);
-    expect(calc.value("   12        -  8   ")).toBe(4);
-    expect(calc.value("142        -9   ")).toBe(133);
-    expect(calc.value("72+  15")).toBe(87);
-    expect(calc.value(" 12*  4")).toBe(48);
-    expect(calc.value(" 50/10 ")).toBe(5);
-    expect(calc.value("2.53")).toBe(2.53);
-    expect(calc.value("4*2.5 + 8.5+1.5 / 3.0")).toBe(19);
-    expect(calc.value("5.0005 + 0.0095")).toBe(5.01);
-    expect(calc.value("67+2")).toBe(69);
-    expect(calc.value(" 2-7")).toBe(-5);
-    expect(calc.value("5*7")).toBe(35);
-    expect(calc.value("8/4")).toBe(2);
-    expect(calc.value("2 -4 +6 -1 -1- 0 +8")).toBe(10);
-    expect(calc.value("1 -1   + 2   - 2   +  4 - 4 +    6")).toBe(6);
-    expect(calc.value(" 2*3 - 4*5 + 6/3 ")).toBe(-12);
-    expect(calc.value("2*3*4/8 -   5/2*4 +  6 + 0/3   ")).toBe(-1);
-    expect(calc.value("10/4")).toBe(2.5);
-    expect(calc.value("5/3")).toBeCloseTo(1.66666);
-    expect(calc.value("3 + 8/5 -1 -2*5")).toBeCloseTo(-6.4);
-    expect(calc.test("  6  + c")).toBe(false);
-    expect(calc.test("  7 & 2")).toBe(false);
-    expect(calc.test(" %  ")).toBe(false);
-    expect(calc.test(" 5 + + 6")).toBe(false);
-    expect(calc.value("5/0")).toBe(Infinity);
-    expect(calc.value("(2)")).toBe(2);
-    expect(calc.value("(5 + 2*3 - 1 + 7 * 8)")).toBe(66);
-    expect(calc.value("(67 + 2 * 3 - 67 + 2/1 - 7)")).toBe(1);
-    expect(calc.value("(2) + (17*2-30) * (5)+2 - (8/2)*4")).toBe(8);
-    expect(calc.value("(5*7/5) + (23) - 5 * (98-4)/(6*7-42)")).toBe(-Infinity);
-    expect(calc.value("(((((5)))))")).toBe(5);
-    expect(calc.value("(( ((2)) + 4))*((5))")).toBe(30);
-    expect(calc.value("(( ((2)) + 4))*((5)  -1) ")).toBe(24);
-    expect(calc.test("2 + (5 * 2")).toBe(false);
-    expect(calc.test("(((((4))))")).toBe(false);
-    expect(calc.test("((((4)))))")).toBe(false);
-    expect(calc.test("((2)) * ((3")).toBe(false);
-    expect(
-      calc.value(
-        " ( (( ( (485.56) -  318.95) *( 486.17/465.96 -  324.49/-122.8 )+ -422.8) * 167.73+-446.4 *-88.31) -271.61/ ( (( 496.31 / ((  -169.3*  453.70) ) )/-52.22 )* (( (-134.9* (-444.1-(( 278.79 * (  -384.5)) ) / (-270.6/  396.89-(  -391.5/150.39-  -422.9 )* -489.2 ) )+-38.02 )) )) )"
-      )
-    ).toBeCloseTo(71470.126502);
-  }
+  expect(calc.value("2 + 3")).toBe(5);
+  expect(calc.value("2 * 3")).toBe(6);
+  expect(calc.value("2 * -3")).toBe(-6);
+  expect(calc.value("89")).toBe(89);
+  expect(calc.value("2.53")).toBe(2.53);
+  expect(calc.value("-1.2")).toBe(-1.2);
+  expect(calc.test("")).toBe(false);
+  expect(calc.test("1 +")).toBe(false);
+  expect(calc.test("(1 +")).toBe(false);
+  expect(calc.value("   12        -  8   ")).toBe(4);
+  expect(calc.value("142        -9   ")).toBe(133);
+  expect(calc.value("72+  15")).toBe(87);
+  expect(calc.value(" 12*  4")).toBe(48);
+  expect(calc.value(" 50/10 ")).toBe(5);
+  expect(calc.value("2.53")).toBe(2.53);
+  expect(calc.value("4*2.5 + 8.5+1.5 / 3.0")).toBe(19);
+  expect(calc.value("5.0005 + 0.0095")).toBe(5.01);
+  expect(calc.value("67+2")).toBe(69);
+  expect(calc.value(" 2-7")).toBe(-5);
+  expect(calc.value("5*7")).toBe(35);
+  expect(calc.value("8/4")).toBe(2);
+  expect(calc.value("2 -4 +6 -1 -1- 0 +8")).toBe(10);
+  expect(calc.value("1 -1   + 2   - 2   +  4 - 4 +    6")).toBe(6);
+  expect(calc.value(" 2*3 - 4*5 + 6/3 ")).toBe(-12);
+  expect(calc.value("2*3*4/8 -   5/2*4 +  6 + 0/3   ")).toBe(-1);
+  expect(calc.value("10/4")).toBe(2.5);
+  expect(calc.value("5/3")).toBeCloseTo(1.66666);
+  expect(calc.value("3 + 8/5 -1 -2*5")).toBeCloseTo(-6.4);
+  expect(calc.test("  6  + c")).toBe(false);
+  expect(calc.test("  7 & 2")).toBe(false);
+  expect(calc.test(" %  ")).toBe(false);
+  expect(calc.test(" 5 + + 6")).toBe(false);
+  expect(calc.value("5/0")).toBe(Infinity);
+  expect(calc.value("(2)")).toBe(2);
+  expect(calc.value("(5 + 2*3 - 1 + 7 * 8)")).toBe(66);
+  expect(calc.value("(67 + 2 * 3 - 67 + 2/1 - 7)")).toBe(1);
+  expect(calc.value("(2) + (17*2-30) * (5)+2 - (8/2)*4")).toBe(8);
+  expect(calc.value("(5*7/5) + (23) - 5 * (98-4)/(6*7-42)")).toBe(-Infinity);
+  expect(calc.value("(((((5)))))")).toBe(5);
+  expect(calc.value("(( ((2)) + 4))*((5))")).toBe(30);
+  expect(calc.value("(( ((2)) + 4))*((5)  -1) ")).toBe(24);
+  expect(calc.test("2 + (5 * 2")).toBe(false);
+  expect(calc.test("(((((4))))")).toBe(false);
+  expect(calc.test("((((4)))))")).toBe(false);
+  expect(calc.test("((2)) * ((3")).toBe(false);
+  expect(
+    calc.value(
+      " ( (( ( (485.56) -  318.95) *( 486.17/465.96 -  324.49/-122.8 )+ -422.8) * 167.73+-446.4 *-88.31) -271.61/ ( (( 496.31 / ((  -169.3*  453.70) ) )/-52.22 )* (( (-134.9* (-444.1-(( 278.79 * (  -384.5)) ) / (-270.6/  396.89-(  -391.5/150.39-  -422.9 )* -489.2 ) )+-38.02 )) )) )"
+    )
+  ).toBeCloseTo(71470.126502);
 });
 
 test("JSON.parse should be correctly reproduced", () => {
